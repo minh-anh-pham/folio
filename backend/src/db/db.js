853336c1cd4b6b109cd27b69d5e875f8e9b898d5
minh-anh-pham <102440587+minh-anh-pham@users.noreply.github.com>
2023-06-04 13:02:11 +0100

    Edit Dockerfile

--- backend/src/db/db.js
+++ backend/src/db/db.js
@@ -1,8 +1,10 @@
 const {Sequelize} = require("sequelize");
 const path = require("path");
+import sqlite3 from "sqlite3";
 
 const db = new Sequelize({
     dialect: 'sqlite',
+    dialectModule: sqlite3,
     storage: path.join(__dirname, "db.sqlite"),
     logging: false
 });
